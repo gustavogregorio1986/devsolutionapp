8f34238de4e07e8727dd3692f35084d4ee4a128a
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-10-01 17:06:12 -0300

    desenvolvimento dfo frontend no home e colocando o footer no home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from "../../footer/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+   imagemEmpresa = 'https://tse4.mm.bing.net/th?id=OIP.O3tnhk070tgDPVeNufsZ3QHaD4&pid=Api&P=0&h=180'
+   empresaAt = 'empresa'
 }
