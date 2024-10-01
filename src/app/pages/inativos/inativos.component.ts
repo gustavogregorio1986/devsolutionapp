40dd1ea2dd6ec6f2c4e4b7aa2183ae694d329599
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-10-01 15:54:16 -0300

    desenvolvimento inicial

---
+++ src/app/pages/inativos/inativos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inativos',
+  standalone: true,
+  imports: [],
+  templateUrl: './inativos.component.html',
+  styleUrl: './inativos.component.css'
+})
+export class InativosComponent {
+
+}
