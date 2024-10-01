ee775be5ae89efd36956bac92ff7cc0c64d6a032
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-10-01 16:28:12 -0300

    criação de classe com nome da empresa

---
+++ src/app/classes/empresa.ts
@@ -0,0 +1,14 @@
+// empresa.ts
+export class Empresa {
+    Id: string;
+    NomeEmpresa: string;
+    Cnpj: string;
+    Setor: string;
+
+    constructor(d: string, nomeEmpresa: string, cnpj: string, setor: string) {
+        this.Id = id;
+        this.NomeEmpresa = nomeEmpresa;
+        this.Cnpj = cnpj;
+        this.Setor = setor;
+    }
+}
