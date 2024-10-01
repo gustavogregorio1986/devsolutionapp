9453e591fc59df0f034d3f59d40563fc98241a06
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-10-01 16:43:40 -0300

    desenvolvimento do service no angular

--- src/app/service/empresa.service.ts
+++ src/app/service/empresa.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Empresa } from '../classes/empresa';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpresaService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:33481'
+
+  constructor(private http: HttpClient) { }
+
+  cadastrar(empresa: Empresa): Observable<Empresa> {
+    return this.http.post<Empresa>(this.apiUrl, empresa);
+  }
 }
