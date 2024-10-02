d64e85e11ff03513d461b3abc7d652729be36799
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-10-02 09:33:39 -0300

    desenvolvimento do cadastro do frontend no angular

--- src/app/classes/empresa.ts
+++ src/app/classes/empresa.ts
@@ -3,12 +3,14 @@ export class Empresa {
     Id: string;
     NomeEmpresa: string;
     Cnpj: string;
+    EnumSituacao: string;
     Setor: string;
 
-    constructor(id: string, nomeEmpresa: string, cnpj: string, setor: string) {
+    constructor(id: string, nomeEmpresa: string, cnpj: string, EnumSituacao: string, setor: string) {
         this.Id = id;
         this.NomeEmpresa = nomeEmpresa;
         this.Cnpj = cnpj;
+        this.EnumSituacao = EnumSituacao;
         this.Setor = setor;
     }
 }
--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+
+  empresaForm!: FormGroup;
+
+  enumSituacoes = [
+    { value: 'Ativo', label: 'Active' },
+    { value: 'Inativo', label: 'Inactive' },
+  ];
+
+  constructor(private fb: FormBuilder){
+       this.empresaForm = this.fb.group({
+        NomeEmpresa: ['', Validators.required],
+        Cnpj:['', Validators.required],
+        EnumSituacao: ['', Validators.required],
+        Setor: ['', Validators.required]
+       })    
+  }
+
+  onSubmit() {
+    if (this.empresaForm.valid) {
+      console.log(this.empresaForm.value);
+    } else {
+      console.log('Formulário inválido');
+    }
+  }
 
 }
